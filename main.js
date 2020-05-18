47c1c54590f91ec2084c8f9eb3d4321255c95871
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-05-17 23:11:45 -0600

    finished styling and adding content

--- main.js
+++ main.js
@@ -5,21 +5,38 @@ const jumbo4 = $("span#fade4");
 const jumbo5 = $("span#fade5");
 const btn = $("#start");
 
+jumbo1.hide();
+jumbo2.hide();
+jumbo3.hide();
+jumbo4.hide();
+jumbo5.hide();
+$(".left").hide();
+$(".right").hide();
+$("footer").hide();
 
 $(document).ready(() => {
-    jumbo1.hide();
-    jumbo2.hide();
-    jumbo3.hide();
-    jumbo4.hide();
-    jumbo5.hide();
-
-    $(btn).on("click", function() {
-        btn.hide();
-        jumbo1.delay(400*0).fadeIn("slow");
-        jumbo2.delay(400*1.5).fadeIn("slow");
-        jumbo3.delay(400*3).fadeIn("slow");
-        jumbo4.delay(400*4.5).fadeIn("slow");
-        jumbo5.delay(400*6).fadeIn("slow");
-    })
-
-})
+  // jumbo1.hide();
+  // jumbo2.hide();
+  // jumbo3.hide();
+  // jumbo4.hide();
+  // jumbo5.hide();
+  // $(".left").hide();
+  // $(".right").hide();
+  $(btn).on("click", function () {
+    btn.hide();
+    jumbo1.delay(400 * 0).fadeIn("slow");
+    jumbo2.delay(400 * 1.5).fadeIn("slow");
+    jumbo3.delay(400 * 3).fadeIn("slow");
+    jumbo4.delay(400 * 4.5).fadeIn("slow");
+    jumbo5.delay(400 * 6).fadeIn("slow");
+    $(".left")
+      .delay(400 * 9)
+      .fadeIn("slow");
+    $(".right")
+      .delay(400 * 9)
+      .fadeIn("slow");
+    $("footer")
+      .delay(400 * 10)
+      .fadeIn("slow");
+  });
+});

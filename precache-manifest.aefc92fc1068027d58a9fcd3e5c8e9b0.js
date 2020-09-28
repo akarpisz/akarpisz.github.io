88b512baa0c5521ebcfb041435946bf478aadc80
andrew karpisz <andrew.karpisz@gmail.com>
2020-09-28 17:10:24 -0600

    Updates

---
+++ precache-manifest.aefc92fc1068027d58a9fcd3e5c8e9b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f12dc758c5d45dd5eff",
+    "url": "/static/css/main.582c96e6.chunk.css"
+  },
+  {
+    "revision": "2f12dc758c5d45dd5eff",
+    "url": "/static/js/main.2f12dc75.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f8fbba6e4580adb6e38dc0ab6cb312cf",
+    "url": "/index.html"
+  }
+];

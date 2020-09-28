8d324599ba1ba765603ba3bc4ed7362ae899181e
andrew karpisz <andrew.karpisz@gmail.com>
2020-09-28 16:30:49 -0600

    style update

---
+++ src/components/Portfolio.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Work:</h1>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          {
+            resumeData.portfolio && resumeData.portfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item" key={item.name}>
+                  {/* <div className="item-wrap"> */}
+                    {/* <a href="#modal-01"> */}
+                      <img src={item.imgurl} alt={item.description} className="item-img"/>
+                      <div 
+                      // className="overlay"
+                      >
+                        <div 
+                        // className="portfolio-item-meta"
+                        >
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+              
+                      </div>
+                    {/* </a> */}
+                    <p><a href={item.github}>Github</a></p>
+              {item.heroku === "" ? <div/> :
+              <p><a href={item.heroku}>Heroku</a></p>
+            }
+                  </div>
+                // </div>
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+  </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,124 @@
+let resumeData = {
+    "imagebaseurl":"https://akarpisz.github.io/",
+    "name": "Andrew Karpisz",
+    "role": "Full-Stack Developer",
+    "linkedinId":"Andrew A.K.",
+    "skypeid": "live:.cid.5336dba42b5837a",
+    "roleDescription": "I graduated from The University of Denver Full-Stack Bootcamp. I love working on the back-end and with databases, and am proficient with the front-end and UX/UI design. In my freetime, I like to explore different frameworks for development and build on the Raspberry Pi and Arduino. I'm looking to start my Master's Degree in Computer Science. ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"www.linkedin.com/in/the-andrew-ak",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/akarpisz",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/TheAndrewAK",
+          "className":"fa fa-twitter"
+        },
+        {
+          "name":"Gmail",
+          "url":"mailto:andrew.karpisz@gmail.com",
+          "className":"fa fa-email"
+        }
+      ],
+    "aboutme":"Full-Stack Web Developer leveraging a background in telecommunications, healthcare, and hospitality to build a better user experience with web-based applications. Recently graduated from the University of Denver with a certificate in Full-Stack Web Development and developed proficiency with NodeJS,  ReactJS, MySQL, MongoDB,  HTML5, CSS3, and other modern web development technologies. I applied aspects of UX and agile development in a recent project where I worked to develop a single-page MERN app that allows users to save and share translations, allowing users to communicate despite language barriers. Every application I assist in developing is a chance to make a lasting positive impact on a user, and improve personally and professionally. My communication skills, ingenuity, and motivation make me a welcomed and valuable asset to any team.",
+    "address":"Denver, CO",
+    "website":"https://akarpisz.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Denver",
+        "specialization":"Full-Stack Web Development",
+        "MonthOfPassing":"Sept",
+        "YearOfPassing":"2020",
+        "Achievements":""
+      },
+      // {
+      //   "UniversityName":"Arapahoe Community College",
+      //   "specialization":"Computer",
+      //   "MonthOfPassing":"Jan",
+      //   "YearOfPassing":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "work":[
+      {
+        "CompanyName":"Comcast",
+        "specialization":"Communications Technician",
+        "MonthOfLeaving":"Sept",
+        "YearOfLeaving":"2020",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Kindred At Home",
+        "specialization":"Personal Caregiver",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2018",
+        "Achievements":"Caregiver of The Month August 2018"
+      }
+    ],
+    "skillsDescription":" MERN Full-Stack Developer",
+    "skills":[
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"NodeJS"
+      },
+      {
+        "skillname":"ExpressJS"
+      },
+      {
+        "skillname":"REST APIs"
+      },
+      {
+        "skillname":"Linux"
+      },
+      {
+        "skillname":"Mongodb & MySQL"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Lyngua",
+        "description":"Lyngua - Translation Saving and Sharing App. Using NodeJS ExpressJS MongoDB/Mongoose and ReactJS",
+        "imgurl":"/images/portfolio/lyngua.png",
+        "github": "https://github.com/akarpisz/lyngua",
+        "heroku": "lyngua.herokuapp.com"
+      },
+      {
+        "name":"Budget-Tracker PWA",
+        "description":"A budget tracking progressive web app. Uses NodeJS MongoDB ExpressJS and IndexedDB ",
+        "imgurl":"/images/BudgetTracker.jpg",
+        "github": "https://github.com/akarpisz/budget-trackerPWA",
+        "heroku": ""
+      },
+      {
+        "name":"Fitness Tracker",
+        "description":"Record and edit workouts",  
+        "imgurl":"images/Fitness.png"
+      },
+      // {
+      //   "name":"project4",
+      //   "description":"mobileapp",
+      //   "imgurl":"images/portfolio/phone.jpg"
+      // }
+    ],
+    "testimonials":[
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // },
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // }
+    ]
+  }
+  
+  export default resumeData
